bcf8ae7b2dc6c97664aff8a797539e4d87aa41b5
saveliy <savelii.moshkota@ext.jumingo.com>
2023-08-23 18:22:57 -0700

    add seed data

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 using Microsoft.EntityFrameworkCore;
 using Core.Entities;
+using System.Reflection;
 
 namespace Infrastructure.Data
 {
@@ -15,6 +16,14 @@ namespace Infrastructure.Data
 
     }
     public DbSet<Product> Products { get; set; }
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
 
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
   }
 }
